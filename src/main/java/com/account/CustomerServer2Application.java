a384872ae53f81e7f4c6f13749d5b601f5a2166d
Vibhu Raj <vibhuraj23@gmail.com>
2022-03-05 22:36:12 +0530

    Cors

--- src/main/java/com/account/CustomerServer2Application.java
+++ src/main/java/com/account/CustomerServer2Application.java
@@ -2,7 +2,8 @@ package com.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
 @SpringBootApplication
 public class CustomerServer2Application {
 
