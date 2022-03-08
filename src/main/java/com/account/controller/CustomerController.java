211aadd5fe23db33bb29e8396d38fce3b98e0c6c
Vibhu Raj <vibhuraj23@gmail.com>
2022-03-08 22:39:02 +0530

    PathVariable and PutMapping

--- src/main/java/com/account/controller/CustomerController.java
+++ src/main/java/com/account/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,8 +57,8 @@ public class CustomerController {
 			return new ResponseEntity(e.getMessage(), HttpStatus.CONFLICT);
 		}
 	}
-	@DeleteMapping("/customer")
-	public ResponseEntity<?> deleteCustomer(@RequestParam Long id ){
+	@DeleteMapping("/customer/{id}")
+	public ResponseEntity<?> deleteCustomer(@PathVariable Long id ){
 		try {
 			this.customerService.deleteCustomer(id);
 			return new ResponseEntity(id+" Deleted Successfully",HttpStatus.OK);
@@ -67,5 +68,12 @@ public class CustomerController {
 		}
 		
 	}
+	@PutMapping("/customer")
+	public ResponseEntity<?> updateCustomer(@RequestBody Customer customer){
+		this.customerRepository.save(customer);
+		return new ResponseEntity(customer.getName()+" updated", HttpStatus.OK);
+	}
+		
+	
 
 }
